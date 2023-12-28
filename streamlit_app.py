a0de07a9435189dee74102a3bca2d775456fabc4
nikAcharya1 <137563632+nikAcharya1@users.noreply.github.com>
2023-12-28 01:38:21 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,26 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
 
-st.write("Remove background from your image")
+st.set_page_config(layout="wide", page_title="Image Background Remover")
+
+st.write("## Remove background from your image")
+st.sidebar.write("## Upload and download :gear:")
+
+MAX_FILE_SIZE = 5 * 1024 * 1024  # 5MB
+
+def fix_image(upload):
+    image = Image.open(upload)
+    col1.write("Original Image :camera:")
+    col1.image(image)
+
+    col2.write("Fixed Image :wrench:")
+
+col1, col2 = st.columns(2)
+my_upload = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+
+if my_upload is not None:
+    if my_upload.size > MAX_FILE_SIZE:
+        st.error("The uploaded file is too large. Please upload an image smaller than 5MB.")
+    else:
+        fix_image(upload=my_upload)
+else:
+    fix_image("./zebra.jpg")
