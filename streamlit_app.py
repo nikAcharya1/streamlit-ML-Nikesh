25b6d44737d18a554f4bdc4d511eb993aebb2bcc
nikAcharya1 <137563632+nikAcharya1@users.noreply.github.com>
2023-12-29 02:11:42 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,18 @@
 import streamlit as st
 from PIL import Image
-!pip install tensorflow
-from tensorflow.keras.models import load_model
-from tensorflow.keras.utils import load_img
-from tensorflow.keras.utils import img_to_array
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
 import numpy as np
 
 # Load pre-trained model
-model = load_model('Model1-Run1.h5')
+model = tf.keras.models.load_model('Model1-Run1.h5')
 
 def preprocess_image(image_path):
     img = Image.open(image_path)
     load_image = load_img(image_path, target_size=(224, 224))
     img_array = img_to_array(load_image)
     img_array = img_array / 255.0
-    img_array = img_array.reshape(1, 224, 224, 3)
+    img_array = np.expand_dims(img_array, axis=0)
     return img_array
 
 def predict_image(image_array):
@@ -36,7 +34,7 @@ def main():
         predictions = predict_image(image_array)
 
         # Display predictions
-        st.write(round(predictions[0][0], 2))
+        st.write(f"Prediction: {round(predictions[0][0], 2)}")
 
 if __name__ == "__main__":
     main()
