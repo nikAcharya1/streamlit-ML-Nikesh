8c6d7aa52dbe72ce6e170d3c615911765eac153a
nikAcharya1 <137563632+nikAcharya1@users.noreply.github.com>
2024-01-04 22:30:14 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,6 @@ from tensorflow.keras.utils import img_to_array
 from keras.applications.vgg16 import preprocess_input
 import numpy as np
 import pandas as pd
-!pip install --upgrade pip
 
 # Load pre-trained model
 model11 = load_model('Model1-Run1.h5')
