1ab1f96c7e73d100a248f81f5b78bff081cadc1e
nikAcharya1 <137563632+nikAcharya1@users.noreply.github.com>
2024-01-16 00:07:24 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ def predict_image(image_array):
     predictions13 = model13.predict(image_array)
     return predictions11, predictions12, predictions13
 
-@st.cache_resource
+@st.cache_resource(experimental_allow_widgets=True)
 def main():
     st.title("Facial Image Classification App")
 
